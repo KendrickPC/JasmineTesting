cd03297aa3428853cebbe1d4a47f83d9d08755d4
KendrickPC <kennethpchang@gmail.com>
2022-05-14 15:50:00 -0700

    Working UI and results for app

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -11,15 +11,26 @@ window.addEventListener('DOMContentLoaded', function() {
       const loanYears = document.querySelector('#loan-years').value;
       const yearlyRate = document.querySelector('#loan-rate').value;
 
-      const str = `Loan Amount: ${loanAmount} Loan Years: ${loanYears} Yearly Rate: ${yearlyRate}`;
-      
+      checkValidInputs(loanAmount, loanYears, yearlyRate);
 
+      const monthlyPayments = calculateMonthlyPayment(loanAmount, loanYears, yearlyRate);
+      const resultingStr = `Your monthly payments are $${monthlyPayments} for a loan amount of $${loanAmount}, loan years of ${loanYears}, and yearly rate of ${yearlyRate}%`;
+      document.querySelector('#monthly-payment').innerText = resultingStr;
     });
   }
-
 });
 
-
+function checkValidInputs(loanAmount, loanYears, yearlyRate) {
+  if (typeof loanAmount !== 'number') {
+    throw new Error("Please enter a valid number for loan amount input")
+  }
+  if (typeof loanYears !== 'number') {
+    throw new Error("Please enter a valid number for loan years")
+  }
+  if (typeof yearlyRate !== 'number') {
+    throw new Error("Please enter a valid number for yearly rate")
+  }
+}
 
 
 function getCurrentUIValues() {
@@ -30,28 +41,8 @@ function getCurrentUIValues() {
   }
 }
 
-
-
 function setupIntialValues() {
-  // Get the inputs from the DOM.
-  const amountUI = document.getElementById('loan-amount');
-  const yearsUI = document.getElementById('loan-years');
-  const loanRateUI = document.getElementById('loan-rate');
-  // Put some default values in the inputs
-  const defaultValues = {
-    amount: 10000,
-    years: 10,
-    rate: 4.5,
-  }
-  // Call a function to calculate the current monthly payment
-  // NO! DO NOT CALL THE calculateMonthlyPayment function! DO NOT! USELESS!
-  /*
-  The directions would have been clearer if they
-  said "load some default values to the UI/screen"
-  */
-  // amountUI.value = defaultValues.amount;
-  // yearsUI.value = defaultValues.years;
-  // loanRateUI.value = defaultValues.rate;
+
 }
 
 // Get the current values from the UI
@@ -63,11 +54,19 @@ function update() {
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
-
+function calculateMonthlyPayment(amount, years, rate) {
+  const monthlyRate = (rate / 100) / 12
+  const numerator = amount * monthlyRate;
+  const n = Math.floor(years * 12);
+  const denominator = (1 - Math.pow((1 + monthlyRate), -n))
+  return (numerator / denominator).toFixed(2);
 }
+
+
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
 }
+
+
